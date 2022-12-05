969345046d04d2b51f3c5668deac630819b73bd0
Paul Visscher <p.e.visscher@skyleague.io>
2022-12-05 13:42:01 +0100

    refactor(lint): remove some unnecessary linting rules from spec files

--- .eslintrc.base.js
+++ .eslintrc.base.js
@@ -108,9 +108,11 @@ module.exports = {
             files: ['*.spec.ts'],
             rules: {
                 '@typescript-eslint/no-explicit-any': 'off',
+                '@typescript-eslint/no-unsafe-assignment': 'off',
                 '@typescript-eslint/no-unsafe-argument': 'off',
                 '@typescript-eslint/no-unsafe-member-access': 'off',
                 '@typescript-eslint/no-var-requires': 'off',
+                '@typescript-eslint/unbound-method': 'off',
             },
         },
         {
