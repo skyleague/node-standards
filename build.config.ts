947a86c7c307365cba4be5724909ad57f26b47af
Paul Visscher <p.e.visscher@skyleague.io>
2023-01-13 14:11:59 +0100

    fix: eslint import order

--- build.config.ts
+++ build.config.ts
@@ -1,23 +1,19 @@
-import { build } from 'esbuild'
+import glob from 'fast-glob'
 
 import { execSync } from 'child_process'
+import fs from 'fs'
 import path from 'path'
 
-// eslint-disable-next-line @typescript-eslint/no-var-requires, @typescript-eslint/no-unsafe-assignment
-const { jsonLoaderPlugin, nodeExternalsPlugin } = require('./esbuild.plugins')
-
 async function main() {
-    execSync('npx tsc -p tsconfig.dist.json --emitDeclarationOnly')
+    execSync(`rm -rf dist`)
+    execSync('npx tsc -p tsconfig.dist.json')
 
-    await build({
-        bundle: true,
-        sourcemap: true,
-        platform: 'node',
-        outfile: path.join(__dirname, '.main.js'),
-        entryPoints: [path.join(__dirname, 'index.ts')],
-        treeShaking: true,
-        plugins: [jsonLoaderPlugin, nodeExternalsPlugin],
-    })
+    const srcDir = path.join(__dirname, 'src')
+    await Promise.all(
+        [...new Set((await glob(path.join(srcDir, '**/*.schema.js'))).map((x) => path.dirname(x)))].map((d) =>
+            fs.promises.cp(d, path.join(__dirname, 'dist', path.relative(srcDir, d)), { recursive: true })
+        )
+    )
 }
 main().catch((err) => {
     console.error(err)
