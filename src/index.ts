2c69e9a6d011f41066ddc89784e5c80afccea2e4
Paul Visscher <p.e.visscher@skyleague.io>
2024-04-10 21:13:01 +0200

    chore: remove eslint

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export * from './config/index.js'
 export * from './lib/index.js'
 export * from './commands/index.js'
 
-import packageJson from '../package.json' assert { type: 'json' }
+import packageJson from '../package.json' with { type: 'json' }
 
 import type { CommandModule } from 'yargs'
 import yargs from 'yargs'
